2ace68c85026210d221ceafb1267d9c52af4018b
vkhatri <vir.khatri@gmail.com>
2014-09-22 09:28:44 +0000

    bump version to 0.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'vir.khatri@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures rssh'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.3'
 provides         'rssh::default'
 
 supports 'debian'
